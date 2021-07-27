4c68754a1496a44e74ecdc2a6e7878bf62493c9c
Trask Stalnaker <trask.stalnaker@gmail.com>
2021-07-27 16:46:57 -0700

    Simplify InstrumentationType

--- instrumentation-api/src/main/java/io/opentelemetry/instrumentation/api/tracer/ClientSpan.java
+++ instrumentation-api/src/main/java/io/opentelemetry/instrumentation/api/tracer/ClientSpan.java
@@ -9,9 +9,9 @@ import io.opentelemetry.api.trace.Span;
 import io.opentelemetry.api.trace.SpanKind;
 import io.opentelemetry.context.Context;
 import io.opentelemetry.context.ContextKey;
-import org.checkerframework.checker.nullness.qual.Nullable;
 import java.util.HashMap;
 import java.util.Map;
+import org.checkerframework.checker.nullness.qual.Nullable;
 
 /**
  * This class encapsulates the context key for storing the current {@link SpanKind#CLIENT} span in
@@ -23,13 +23,15 @@ public final class ClientSpan {
   private static final ContextKey<Span> KEY =
       ContextKey.named("opentelemetry-traces-client-span-key");
 
-  private static final Map<Integer, ContextKey<Span>> instrumentationKeys = new HashMap<>();
+  private static final Map<InstrumentationType, ContextKey<Span>> instrumentationKeys =
+      new HashMap<>();
 
   static {
-    instrumentationKeys.put(InstrumentationType.HTTP.getValue(), createTypedKey(InstrumentationType.HTTP));
-    instrumentationKeys.put(InstrumentationType.DB.getValue(), createTypedKey(InstrumentationType.DB));
-    instrumentationKeys.put(InstrumentationType.RPC.getValue(), createTypedKey(InstrumentationType.RPC));
-    instrumentationKeys.put(InstrumentationType.MESSAGING.getValue(), createTypedKey(InstrumentationType.MESSAGING));
+    instrumentationKeys.put(InstrumentationType.HTTP, createTypedKey(InstrumentationType.HTTP));
+    instrumentationKeys.put(InstrumentationType.DB, createTypedKey(InstrumentationType.DB));
+    instrumentationKeys.put(InstrumentationType.RPC, createTypedKey(InstrumentationType.RPC));
+    instrumentationKeys.put(
+        InstrumentationType.MESSAGING, createTypedKey(InstrumentationType.MESSAGING));
   }
 
   /** Returns true when a {@link SpanKind#CLIENT} span is present in the passed {@code context}. */
@@ -43,7 +45,7 @@ public final class ClientSpan {
       return false;
     }
 
-    ContextKey<Span> contextKey = instrumentationKeys.get(instrumentationType.getValue());
+    ContextKey<Span> contextKey = instrumentationKeys.get(instrumentationType);
     if (contextKey == null) {
       return false;
     }
@@ -60,14 +62,14 @@ public final class ClientSpan {
     return context.get(KEY);
   }
 
-   public static Context with(Context context, Span clientSpan, InstrumentationType instrumentationType) {
+  public static Context with(
+      Context context, Span clientSpan, InstrumentationType instrumentationType) {
 
     if (!instrumentationType.isNone()) {
-      ContextKey<Span> typedKey = instrumentationKeys.get(instrumentationType.getValue());
+      ContextKey<Span> typedKey = instrumentationKeys.get(instrumentationType);
       if (typedKey == null) {
-        typedKey = ContextKey
-            .named("opentelemetry-traces-client-span-key-" + instrumentationType.getValue());
-        instrumentationKeys.put(instrumentationType.getValue(), typedKey);
+        typedKey = ContextKey.named("opentelemetry-traces-client-span-key-" + instrumentationType);
+        instrumentationKeys.put(instrumentationType, typedKey);
       }
 
       context = context.with(typedKey, clientSpan);
@@ -79,6 +81,6 @@ public final class ClientSpan {
   private ClientSpan() {}
 
   private static ContextKey<Span> createTypedKey(InstrumentationType type) {
-    return ContextKey.named("opentelemetry-traces-client-span-key-" + type.getValue());
+    return ContextKey.named("opentelemetry-traces-client-span-key-" + type);
   }
 }
--- instrumentation-api/src/main/java/io/opentelemetry/instrumentation/api/tracer/InstrumentationType.java
+++ instrumentation-api/src/main/java/io/opentelemetry/instrumentation/api/tracer/InstrumentationType.java
@@ -1,38 +1,31 @@
 package io.opentelemetry.instrumentation.api.tracer;
 
 public class InstrumentationType {
-  private static final int NONE_VALUE = 0;
-  private static final int HTTP_VALUE = 1;
-  private static final int RPC_VALUE = 2;
-  private static final int DB_VALUE = 3;
-  private static final int MESSAGING_VALUE = 4;
 
-  public static final InstrumentationType NONE = new InstrumentationType(NONE_VALUE);
-  public static final InstrumentationType HTTP = new InstrumentationType(HTTP_VALUE);
-  public static final InstrumentationType RPC = new InstrumentationType(RPC_VALUE);
-  public static final InstrumentationType DB = new InstrumentationType(DB_VALUE);
-  public static final InstrumentationType MESSAGING = new InstrumentationType(MESSAGING_VALUE);
+  public static final InstrumentationType NONE = new InstrumentationType("none");
+  public static final InstrumentationType HTTP = new InstrumentationType("http");
+  public static final InstrumentationType RPC = new InstrumentationType("rpc");
+  public static final InstrumentationType DB = new InstrumentationType("db");
+  public static final InstrumentationType MESSAGING = new InstrumentationType("messaging");
 
-  private final int value;
+  private final String name;
 
-  public InstrumentationType create(int value) {
-    switch (value) {
-      case NONE_VALUE: return NONE;
-      case HTTP_VALUE: return HTTP;
-      case RPC_VALUE: return RPC;
-      case DB_VALUE: return DB;
-      case MESSAGING_VALUE: return MESSAGING;
-      default: return new InstrumentationType(value);
-    }
+  // each call to this method creates a unique instrumentation type
+  // name is only used for debugging
+  public static InstrumentationType create(String name) {
+    return new InstrumentationType(name);
   }
 
-  private InstrumentationType(int value) {
-    this.value = value;
+  private InstrumentationType(String name) {
+    this.name = name;
   }
 
-  public boolean isNone() { return value == NONE_VALUE; }
+  public boolean isNone() {
+    return this == NONE;
+  }
 
-  public int getValue(){
-    return value;
+  @Override
+  public String toString() {
+    return name;
   }
 }
