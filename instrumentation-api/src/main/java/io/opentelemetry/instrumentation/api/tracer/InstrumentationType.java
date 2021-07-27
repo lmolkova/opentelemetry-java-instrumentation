3131ea9b2d8a104b069c6b0fa2e8a43b0a3a09ab
Liudmila Molkova <limolkova@microsoft.com>
2021-07-27 14:16:19 -0700

    new file

---
+++ instrumentation-api/src/main/java/io/opentelemetry/instrumentation/api/tracer/InstrumentationType.java
@@ -0,0 +1,36 @@
+package io.opentelemetry.instrumentation.api.tracer;
+
+public class InstrumentationType {
+  static final int NONE = 0;
+  static final int HTTP = 1;
+  static final int RPC =2;
+  static final int DB =3;
+  static final int MESSAGING = 4;
+
+  public static final InstrumentationType NONE_TYPE = new InstrumentationType(NONE);
+  public static final InstrumentationType HTTP_TYPE = new InstrumentationType(HTTP);
+  public static final InstrumentationType RPC_TYPE = new InstrumentationType(RPC);
+  public static final InstrumentationType DB_TYPE = new InstrumentationType(DB);
+  public static final InstrumentationType MESSAGING_TYPE = new InstrumentationType(MESSAGING);
+  private final int value;
+
+  // TODO dup
+  public InstrumentationType create(int value) {
+    switch (value) {
+      case NONE: return NONE_TYPE;
+      case HTTP: return HTTP_TYPE;
+      case RPC: return RPC_TYPE;
+      case DB: return DB_TYPE;
+      case MESSAGING: return MESSAGING_TYPE;
+      default: return new InstrumentationType(value);
+    }
+  }
+
+  private InstrumentationType(int value) {
+    this.value = value;
+  }
+
+  public int getValue(){
+    return value;
+  }
+}
